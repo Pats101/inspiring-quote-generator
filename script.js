6c838973558659db708624883804bae3ca056e3f
Patrick Tonderai Ganhiwa <ptganhiwa8@gmail.com>
2022-01-26 19:03:33 +0200

    making updates to the scripts and css

--- script.js
+++ script.js
@@ -3,12 +3,27 @@
  const authorText = document.getElementById('author');
  const twitterBtn = document.getElementById('twitter');
  const newQuoteBtn = document.getElementById('new-quote');
+ const loader = document.getElementById('loader');
 
  
  let apiQuotes = [];         //difference between const and let is that const is when the value is never changing
 
+ // Show Loading
+ function loading() {
+     loader.hidden = false;
+     quoteContainer.hidden = true;
+ }
+
+
+ // Hide Loading
+ function complete() {
+     quoteContainer.hidden = false;
+     loader.hidden = true;
+ }
+
 // Show New Quote
 function newQuote() {
+    loading();
     //Pick a random quote from apiQuotes array
     //use Math.floor to whole number
     //Multiply with length of the apiQuotes so that the number is never bigger than it
@@ -30,12 +45,14 @@ function newQuote() {
         quoteText.classList.remove('long-quote');
     }
 
-    // Set text content with the value 
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl); //response will not be populated until quotes are fetched from the api
